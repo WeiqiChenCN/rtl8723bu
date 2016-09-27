384d5ee57ae8c47d6d1c5d490910da3d209e6292
Weiqi Chen <weiqi_chen@outlook.com>
2016-09-27 23:25:39 +0800

    Update code.

---
+++ init.c
@@ -0,0 +1,21 @@
+
+static int __init rtl_entry(void){
+
+	int ret=0;
+	printk(   )
+	//init variable， alloc memory , other stuff.
+	//It's ok that no target device when insmod.
+
+	//and then register usb_driver.
+
+
+
+	return 0;
+
+
+}
+
+static void __exit rtl_exit(void){
+
+
+}
---
+++ rtl8723usb.c
@@ -0,0 +1,25 @@
+
+#include "rtl_common.h"
+
+
+#include <linux/usb.h>
+#include <linux/mod_devicetable.h>
+
+
+//usb ID 0bda:b720 Realtek Semiconductor Corp.
+
+
+struct usb_device_id rtl_usb_id_table[] = {
+	{ USB_DEVICE( 0x0bda, 0xb720 ), .driver_info=0xffff },
+
+	{ 0, },
+
+};
+
+MODULE_AUTHOR(DRV_PROJECT);
+MODULE_VERSION("0.0.0");
+MODULE_DESCRIPTION("Realtek RTL8723 USB Wireless LAN driver.");
+MODULE_SUPPORTED_DEVICE("Realtek RTL8723 USB chipset based cards");
+MODULE_DEVICE_TABLE( usb, rtl_usb_id_table );
+MODULE_FIRMWARE(FIRMWARE_RTL8723);
+MODULE_LICENSE("GPL");
